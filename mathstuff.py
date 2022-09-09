953861981b949f68c0f723fbe84e4e57bbe3bb6f
Oskey999 <osacr.rafiq@gmail.com>
2022-09-09 13:51:57 +0800

    fixed spacing

--- mathstuff.py
+++ mathstuff.py
@@ -3,7 +3,7 @@ import math
 def chebyshev(n, start, end):
     listx=[]
     for j in range(n):
-        listx.append(0.5*(start+end)-0.5*(end-start)*math.cos((2*j)*math.pi/6))
+        listx.append(0.5*(start+end)-0.5*(end-start)*math.cos((2*j+1)*math.pi/6))
     print("Chebyshev spacing is:")
     print("in range-",start,":",end)
     print(listx)
@@ -11,5 +11,8 @@ def chebyshev(n, start, end):
 
 # express in terms of list of points from chebyshev
 # k1cos(theta2)+k2cos(theta4)+k3= cos(theta2-theta4)
+#k1=l1/l4
+#k2=l1/l2
+#k3= 
 def freudstien():
     return 0
