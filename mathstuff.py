282cf92ad673b98d2c6a1f0e5bd15007fb2959c9
Oskey999 <osacr.rafiq@gmail.com>
2022-09-09 11:31:11 +0800

    added freud shell

--- mathstuff.py
+++ mathstuff.py
@@ -4,7 +4,12 @@ def chebyshev(n, start, end):
     listx=[]
     for j in range(n):
         listx.append(0.5*(start+end)-0.5*(end-start)*math.cos((2*j)*math.pi/6))
-    print("Chebyshcev spacing is:")
+    print("Chebyshev spacing is:")
     print("in range-",start,":",end)
     print(listx)
     return listx
+
+# express in terms of list of points from chebyshev
+# k1cos(theta2)+k2cos(theta4)+k3= cos(theta2-theta4)
+def freudstien():
+    return 0
